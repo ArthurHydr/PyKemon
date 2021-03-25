c139b775757a767cfe19ee6cf08b77054b4602fd
Arthur Ottoni Ribeiro <61481946+ArthurHydr@users.noreply.github.com>
2021-03-25 12:22:40 -0300

    bug fix

--- main.py
+++ main.py
@@ -7,26 +7,26 @@ from pessoa import *
 def escolher_pokemon_inicial(player):
     print(f"Olá {player}, você poderá escolher agora o Pokemon que irá lhe acompanhar nessa jornada!")
 
-    bulbasaur = PokemonPlanta("Bulbasaur", level=1)
+    bulbasaur = PokemonEletrico("Pikachu", level=1)
     charmander = PokemonFogo("Charmander", level=1)
     squirtle = PokemonAgua("Squirtle", level=1)
 
     print("Você possui 3 escolhas: ")
-    print("1 -", charmander)
-    print("2 -", squirtle)
-    print("3 -", bulbasaur)
+    print("1 -", pikachu)
+    print("2 -", charmander)
+    print("3 -", squirtle)
 
     while True:
         escolha = input("Escolha o seu Pokemon: ")
 
         if escolha == "1":
-            player.capturar(charmander)
+            player.capturar(pikachu)
             break
         elif escolha == "2":
-            player.capturar(squirtle)
+            player.capturar(charmander)
             break
         elif escolha == "3":
-            player.capturar(bulbasaur)
+            player.capturar(squirtle)
             break
         else:
             print("Escolha inválida")
